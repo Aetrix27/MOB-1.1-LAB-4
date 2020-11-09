690dcb80a02cb99ba41fcd6ed16ff05261bc92ec
Aetrix27 <davedogg323@gmail.com>
2020-11-08 23:10:34 -0800

    Completed third page

--- Lab4.playground/Pages/Structs - App Exercise.xcplaygroundpage/Contents.swift
+++ Lab4.playground/Pages/Structs - App Exercise.xcplaygroundpage/Contents.swift
@@ -6,10 +6,19 @@ import Foundation
 For most apps you'll need to have a data structure to hold information about a user. Create a `User` struct that has properties for basic information about a user. At a minimum, it should have properties to represent a user's name, age, height, weight, and activity level. You could do this by having `name` be a `String`, `age` be an `Int`, `height` and `weight` be of type `Double`, and `activityLevel` be an `Int` that will represent a scoring 1-10 of how active they are. Implement this now.
 */
 
+struct User{
+    var name : String
+    var age : Int
+    var height : Double
+    var weight : Double
+    var activityLevel : Int
+}
 
 /*:
 Create a variable instance of `User` and call it your name. Use the memberwise initializer to pass in information about yourself. Then print out a description of your `User` instance using the instance's properties.
 */
+
+var me = User(name : "David", age : 21, height : 6, weight : 130, activityLevel: 8)
 
 
 /*:
@@ -18,16 +27,27 @@ Distance can be represented using a variety of units of measurement. Create a `D
 - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
 */
 
+struct Distance {
+    var feet: Double
+    init(fromMeters meters: Double) {
+        feet = meters * 0.32804
+    }
+
+}
 
 /*:
 Now create an instance of `Distance` called `mile`. Use the initializer for meters to set the distance to 1600. Print out the property for feet and verify that it is equal to 5249.344.
 */
 
+let mile = Distance(fromMeters: 1600)
+print(mile.feet)
 
 /*:
 Now create another instance of `Distance` and give it some other distance. Ensure that both properties are set correctly.
 */
 
+let mile2 = Distance(fromMeters: 1800)
+print(mile2.feet)
 
 /*:
 ## Mile Times and Congratulations
@@ -37,23 +57,49 @@ The `RunningWorkout` struct below holds information about your users' running wo
 Create an instance of `RunningWorkout` and print the `averageMileTime` property. Check that it works properly.
 */
 struct RunningWorkout {
-   var distance: Double
-   var time: Double
-   var elevation: Double
+    var distance: Double
+    var time: Double
+    var elevation: Double
+    var averageMileTime : Double{
+        return distance/time
+        
+    }
+
 }
+
+var work1 = RunningWorkout(distance: 14, time: 5, elevation: 5)
+print(work1.averageMileTime)
+
 /*:
 In other app exercises, you've provided encouraging messages to the user based on how many steps they've completed. A great place to check whether or not you should display something to the user is in a property observer.
 
 In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
 */
+
 struct Steps {
-   var steps: Int
+    var steps: Int{
+        willSet {
+            if(steps == goal){
+                updateUI(msg: "Congratulations!")
+            
+            }
+        }
+
+    }
    var goal: Int
    
-   mutating func takeStep() {
-       steps += 1
+    mutating func takeStep() {
+        steps += 1
+
    }
 }
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var mySteps = Steps(steps: 9999, goal : 10000)
+mySteps.takeStep()
 
 /*:You can refer to this resource to read about Property Observers:
  https://www.hackingwithswift.com/read/0/17/properties
